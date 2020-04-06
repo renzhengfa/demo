7fee225993c0f3177192275935e79260e1c3ab63
renzhengfa <renzhengfa11@qq.com>
2020-04-06 13:55:57 +0800

    HelloWord.java commit

---
+++ HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord{
+    public static void main(String[] args){
+        System.out.println("HelloWord");
+        System.out.println("1");
+        System.out.println("HelloWord");
+        System.out.println("2");
+    }
+}
